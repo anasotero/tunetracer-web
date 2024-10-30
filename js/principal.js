a2a720adba45eec8a9d1a6c511c34bd40f5f5a74
theusplay <2022325768@ifam.edu.br>
2024-10-30 08:31:53 -0400

    Telas de Teclado e Flauta

--- js/principal.js
+++ js/principal.js
@@ -14,11 +14,27 @@ flauta.addEventListener('click', function() {
     showSection('acorde-flauta');
 });
 
-// Seleciona o ícone de som e o áudio
-var iconeSom = document.getElementById("iconeSom");
-var audio = document.getElementById("audio-som");
+// Seleciona o ícone de som e o áudio do C violão 
+var iconeSom_c_violao = document.getElementById("iconeSom-c-violao");
+var audio_c_violao = document.getElementById("audio-som-c-violao");
 
-// Adiciona um evento de clique ao ícone de som
-iconeSom.addEventListener('click', function() {
-    audio.play(); // Reproduz o áudio
+// Seleciona o ícone de som e o áudio do C teclado
+var iconeSom_c_teclado = document.getElementById("iconeSom-c-teclado");
+var audio_c_teclado = document.getElementById("audio-som-c-teclado");
+
+// Seleciona o ícone de som e o áudio do C flauta 
+var iconeSom_c_flauta = document.getElementById("iconeSom-c-flauta");
+var audio_c_flauta = document.getElementById("audio-som-c-flauta");
+
+// Adiciona um evento de clique ao ícone de som do violão nota C
+iconeSom_c_violao.addEventListener('click', function() {
+    audio_c_violao.play(); // Reproduz o áudio
+});
+// Adiciona um evento de clique ao ícone de som do teclado nota C
+iconeSom_c_teclado.addEventListener('click', function() {
+    audio_c_teclado.play(); // Reproduz o áudio
+});
+// Adiciona um evento de clique ao ícone de som da flauta nota C
+iconeSom_c_flauta.addEventListener('click', function() {
+    audio_c_flauta.play(); // Reproduz o áudio
 });
